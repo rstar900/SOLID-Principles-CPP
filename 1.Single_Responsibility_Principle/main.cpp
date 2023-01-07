92f61340f284f9e9bbcd91ef0720642ca08a02c5
Rachit Garg <rachitgarg900@gmail.com>
2023-01-07 23:37:17 +0100

    Add Single responsibility description to main.cpp

--- 1.Single_Responsibility_Principle/main.cpp
+++ 1.Single_Responsibility_Principle/main.cpp
@@ -1,3 +1,9 @@
+/*
+According to Single responsibilty principle, 
+each class do one thing and only have one reason to
+change (i.e. the served functionality)
+*/
+
 #include <iostream>
 
 int main()
