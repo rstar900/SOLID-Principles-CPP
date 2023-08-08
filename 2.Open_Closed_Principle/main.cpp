4f34ccee49acb06f12a7396e5ce095a5e06176cc
Rachit Garg <rachitgarg900@gmail.com>
2023-08-08 23:59:58 +0200

    Added inspiration for Open_Closed_Principle

--- 2.Open_Closed_Principle/main.cpp
+++ 2.Open_Closed_Principle/main.cpp
@@ -1,6 +1,10 @@
 /*
-According to Open Closed Principle, classes are open to extension
-but closed to modification
+Inspiration taken from : 
+https://www.vishalchovatiya.com/open-closed-principle-in-cpp-solid-as-a-rock/
+
+According to Open Closed Principle, 
+classes are open to extension 
+but closed to modification.
 */
 
 #include <iostream>
