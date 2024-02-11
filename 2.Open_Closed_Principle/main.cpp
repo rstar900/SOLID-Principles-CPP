620f3f1b792df0fc658a5050d8a4e19a519e3997
Rachit Garg <rachitgarg900@gmail.com>
2024-02-12 00:18:23 +0100

    Added Visitor Pattern TODO

--- 2.Open_Closed_Principle/main.cpp
+++ 2.Open_Closed_Principle/main.cpp
@@ -5,6 +5,8 @@ https://www.vishalchovatiya.com/open-closed-principle-in-cpp-solid-as-a-rock/
 According to Open Closed Principle, 
 classes are open to extension 
 but closed to modification.
+
+TODO: Maybe add example for visitor pattern
 */
 
 #include <iostream>
